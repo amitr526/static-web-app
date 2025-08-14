b6b4b01f15310e91f54ef68d07e5f2c8a9cdf962
amitr526 <AMITR@sundhedsdata.dk>
2025-08-14 12:47:47 +0200

    Use bicep comment syntax

--- infra/download.sh
+++ infra/download.sh
@@ -3,6 +3,6 @@
 VERSION="refs/heads/master"
 URL="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/${VERSION}/quickstarts/microsoft.documentdb/cosmosdb-sql-serverless/main.bicep"
 
-echo "# Downloaded from ${URL} on $(date) using infra/download.sh" > infra/main.bicep
+echo "// # Downloaded from: [${URL}] on: [$(date)] using infra/download.sh" > infra/main.bicep
 curl -L "${URL}" >> infra/main.bicep
 echo OK
