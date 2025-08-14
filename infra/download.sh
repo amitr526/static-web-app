061f385a279a122bae6aff71ac9354e3a9d9c784
amitr526 <AMITR@sundhedsdata.dk>
2025-08-14 12:45:43 +0200

    Fix bicep URL

--- infra/download.sh
+++ infra/download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION="master"
-URL="https://github.com/Azure/azure-quickstart-templates/blob/${VERSION}/quickstarts/microsoft.documentdb/cosmosdb-sql-serverless/main.bicep"
+VERSION="refs/heads/master"
+URL="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/${VERSION}/quickstarts/microsoft.documentdb/cosmosdb-sql-serverless/main.bicep"
 
 echo "# Downloaded from ${URL} on $(date) using infra/download.sh" > infra/main.bicep
 curl -L "${URL}" >> infra/main.bicep
